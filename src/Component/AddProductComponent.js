bd232605b7fd6c6fac85e17167caaf92e489c95f
Srishti Chouksey <srishtichouksey175@gmail.com>
2018-11-30 20:04:36 +0530

    Second Commit

--- src/Component/AddProductComponent.js
+++ src/Component/AddProductComponent.js
@@ -1,10 +1,37 @@
 import React , { Component } from 'react';
 
 export default class AddProduct extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			ProductInfo:[]
+		}
+		this.addDetail = this.addDetail.bind(this);
+		
+	}
+
 
 	render() {
 		return(
-			<h2>AddProduct</h2>
+			<form id="product" onSubmit= {this.addDetail}>
+				<h3>Add Product Form</h3>
+				<fieldset>
+					Product Name : <input placeholder="Product Name" type="text" />
+				</fieldset>
+				<fieldset>
+					Product Details : <input placeholder="Product Details" type="textbox" />
+				</fieldset>
+				<fieldset>
+					Product Type : <input placeholder="Product Type" type="text" />
+				</fieldset>
+				<fieldset>
+					Product Price : <input placeholder="Product price" type="price" />
+				</fieldset>
+				<fieldset>
+					<input type="submit" />
+				</fieldset>
+			</form>
 		);
 	}
 }
